7b6456177d27383a8979b8e1ef3284079182ea8b
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2021-10-24 18:37:27 +0200

    basic

--- blocks.h
+++ blocks.h
@@ -12,21 +12,22 @@ static const Block blocks[] = {
 	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
 	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
 	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
-	{"",	"sb-price btc Bitcoin 💰",				9000,	21},
+	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
 	/* {"",	"sb-torrent",	20,	7}, */
-	{"",	"sb-memory",	10,	14},
 	/* {"",	"sb-cpu",		10,	18}, */
-	{"",	"sb-cpu-juan",		10,	18},
 	/* {"",	"sb-disk",		0,	18}, */
-	{"",	"sb-disk /home",		0,	18},
-	{"",	"sb-disk /mnt/hdd",	0,	18},
+	/* {"",	"sb-disk /home",		0,	18}, */
+	/* {"",	"sb-disk /mnt/hdd",	0,	18}, */
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	/* {"",	"sb-forecast",	18000,	5}, */
 	/* {"",	"sb-mailbox",	180,	12}, */
 	/* {"",	"sb-nettraf",	1,	16}, */
+	{"",	"sb-kernel",	0,	0},
+	{"",	"sb-memory",	10,	14},
+	{"",	"sb-cpu-juan",		10,	18},
+	{"",	"sb-battery",	5,	3},
 	{"",	"sb-volume",	0,	10},
-	/* {"",	"sb-battery",	5,	3}, */
-	{"",	"sb-clock",	60,	1},
+  {"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
 	/* {"",	"sb-help-icon",	0,	15}, */
 };
